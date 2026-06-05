26f48a66eb7352a82e93290547a3994bddf4b96e
rinbs1120 <76205418@qq.com>
2026-06-05 10:29:24 +0800

    feat: add sitemap reference to robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,5 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/_next/', '/static/'],
     },
+    sitemap: 'https://pixcraftx.com/sitemap.xml',
   };
 }
